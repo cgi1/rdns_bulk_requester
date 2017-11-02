bf62192458265550a64bbd06e7a6d0aa3371b596
cgi1 <gies0r@gmail.com>
2017-11-02 13:47:46 +0100

    Refactoring, support different options via argparse...

--- create_nirsoft_data.py
+++ create_nirsoft_data.py
@@ -1,5 +1,5 @@
 """
-A small script to bulk query
+A small script to bulk query rdns
 @author: Christoph Giese <cgi1>
 """
 
@@ -9,6 +9,9 @@ import socket
 import sys, csv, operator
 import logging
 import datetime
+import argparse
+
+import validators
 
 
 def init_logging():
@@ -27,6 +30,27 @@ def init_logging():
     rootLogger.addHandler(consoleHandler)
     return rootLogger
 
+def read_arguments_from_cli():
+    parser = argparse.ArgumentParser(description='Read 1..n files with ip addresses and do reverse dns lookups')
+    parser.add_argument('-if', '--file-input', dest="file_input",
+                        help='File to read (only line separated files are supported)',
+                        required=False)
+    parser.add_argument('-id', '--dir-input', dest="dir_input",
+                        help='Directory to read (only csv files from nirsoft are supported. See download_nirsoft_lists.sh) (Default: input_data)',
+                        required=False, default='input_data')
+
+    parser.add_argument('-ns', '--net-start', dest="net_start",
+                        help='Network to scan: Start (e.g. 82.132.128.0)',
+                        required=False)
+    parser.add_argument('-ne', '--net-end', dest="net_end",
+                        help='Network to scan: Start (e.g. 82.132.255.255)',
+                        required=False)
+
+    args = vars(parser.parse_args())
+
+    return args
+
+
 def get_ips_in_range(start, end):
     # Thanks to https://stackoverflow.com/questions/819355/how-can-i-check-if-an-ip-is-in-a-network-in-python
     import socket, struct
@@ -34,28 +58,32 @@ def get_ips_in_range(start, end):
     end = struct.unpack('>I', socket.inet_aton(end))[0]
     return [socket.inet_ntoa(struct.pack('>I', i)) for i in range(start, end)]
 
+def rate_from_nl_separtaed_file(file):
 
-def rate_list_of_networks(networks_to_rate, complete=False):
-    for network_to_rate in networks_to_rate:
-        ips_in_range = get_ips_in_range(start=network_to_rate['NET_START'], end=network_to_rate['NET_END'])
-        if not complete:
-            a_thousand_random_ips = random.sample(ips_in_range, 1000)
-        else:
-            a_thousand_random_ips = ips_in_range
-        for random_ip in a_thousand_random_ips:
-            try:
-                host_by_addr = socket.gethostbyaddr(random_ip)
-                print(host_by_addr)
-                logging.debug(host_by_addr)
-            except socket.herror:
-                print("Unknown host(%s)" % random_ip)
+    networks_to_rate = []
 
-init_logging()
+    with open(file) as fin:
+        for line in fin:
 
-def rate_from_csv_files():
+            line = line.rstrip()
+
+            if validators.ipv4(line):
+                networks_to_rate.append(line)
+            else:
+                logging.error("Line (%s) is not a valid IP." %line)
+
+    logging.debug("Start to query rdns for (%s) IPs" % len(networks_to_rate))
+
+    for ip in networks_to_rate:
+        do_query(ip=ip)
+
+
+
+
+def rate_from_csv_files(dir):
     largest_networks = []
 
-    for filename in os.listdir(os.getcwd()):
+    for filename in os.listdir(os.getcwd() + dir + '/'):
 
         if not filename.startswith('networks_'):
             continue
@@ -79,8 +107,45 @@ def rate_from_csv_files():
     rate_list_of_networks(networks_to_rate=largest_networks)
 
 
+def rate_list_of_networks(networks_to_rate, complete=False):
+    for network_to_rate in networks_to_rate:
+        ips_in_range = get_ips_in_range(start=network_to_rate['NET_START'], end=network_to_rate['NET_END'])
+        if not complete:
+            a_thousand_random_ips = random.sample(ips_in_range, 1000)
+        else:
+            a_thousand_random_ips = ips_in_range
+        for random_ip in a_thousand_random_ips:
+            do_query(ip=random_ip)
 
-#rate_from_csv_files()
-networks_to_rate = []
-networks_to_rate.append({'country': 'GB', 'NET_START': '82.132.128.0', 'NET_END': '82.132.255.255', 'TOTAL_IPS': float(32768), 'OWNER': 'o2'})
-rate_list_of_networks(networks_to_rate, complete=True)
+
+def do_query(ip):
+    try:
+        host_by_addr = socket.gethostbyaddr(ip)
+        print(host_by_addr)
+        logging.debug(host_by_addr)
+    except socket.herror:
+        print("Unknown host(%s)" % ip)
+
+
+def main(args, logger):
+
+    if 'net_start' in args and args['net_start'] is not None and 'net_end' in args and args['net_end'] is not None:
+
+        networks_to_rate = []
+        networks_to_rate.append(
+            {'country': 'GB', 'NET_START': args['net_start'], 'NET_END': args['net_end'], 'TOTAL_IPS': float(12345), # ToDo: total IPs.. Not needed here.
+             'OWNER': 'o2'})
+        rate_list_of_networks(networks_to_rate=networks_to_rate, complete=True)
+
+    elif 'file_input' in args and args['file_input'] is not None:
+        rate_from_nl_separtaed_file(file=args['file_input'])
+
+    elif 'dir_input' in args and args['dir_input'] is not None:
+        rate_from_csv_files(dir=args['dir_input'])
+
+if __name__ == '__main__':
+    # Only
+    logger = init_logging()
+    args = read_arguments_from_cli()
+    main(args=args, logger=logger)
+
